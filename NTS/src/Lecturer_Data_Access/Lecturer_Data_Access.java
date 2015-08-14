d88db3ff494294bfbd0fb0b4fc530c1b0260c21a
Keetmalin <keetmalin.13@cse.mrt.ac.lk>
2015-08-14 16:23:00 -0700

    Subject Class Fixed -  with Database

--- NTS/src/Lecturer_Data_Access/Lecturer_Data_Access.java
+++ NTS/src/Lecturer_Data_Access/Lecturer_Data_Access.java
@@ -8,6 +8,7 @@ package Lecturer_Data_Access;
 import Connection.DBConnector;
 import Lecturer_Domain.Lecturer;
 import Student_Domain.Student;
+import Subject_Data_Access.Subject_Data_Access;
 import Subject_Domain.Subject;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -75,7 +76,22 @@ public class Lecturer_Data_Access {
         //create new student object
         Lecturer lecturer = new Lecturer((int) v.get(0), (String) v.get(1), (String) v.get(2), (String) v.get(3),
                 (String) v.get(4), (Date) v.get(5));
-
+        
+        //this is to get the subject list from the bridge class
+        sql = "SELECT * FROM lecturer_has_subject WHERE lecturer_id LIKE '%" + id + "%'";
+        //result set to get selected row of the database
+        ResultSet rs2 = connector.getQuerry(sql);
+        LinkedList<Subject> list = new LinkedList<Subject>();
+        DBConnector db = new DBConnector();
+        Subject_Data_Access sda= new Subject_Data_Access(db);
+        
+        while (rs2.next()) {
+            
+            String code = rs2.getString(2);
+            list.add(sda.getSubjectByCode(code));
+        }
+        lecturer.setSubjects(list);        
+        
         return lecturer;
     }
 
@@ -85,11 +101,15 @@ public class Lecturer_Data_Access {
 
         //result set to get selected row of the database
         ResultSet rs = connector.getQuerry(sql);
-
+        
+        //this wil store the Id of the lecturer - to find the subject list
+        int lecturerId=0;
+        
         //create vector to add each field in the student table
         Vector v = new Vector();
         while (rs.next()) {
-            v.add(rs.getInt("lecturer_id"));
+            lecturerId = rs.getInt("lecturer_id");
+            v.add(lecturerId);
             v.add(rs.getString("name"));
             v.add(rs.getString("picture"));
             v.add(rs.getString("nic"));
@@ -100,6 +120,23 @@ public class Lecturer_Data_Access {
         //create new student object
         Lecturer lecturer = new Lecturer((int) v.get(0), (String) v.get(1), (String) v.get(2), (String) v.get(3),
                 (String) v.get(4), (Date) v.get(5));
+        
+        
+        
+        //this is to get the subject list from the bridge class
+        sql = "SELECT * FROM lecturer_has_subject WHERE lecturer_id LIKE '%" + lecturerId + "%'";
+        //result set to get selected row of the database
+        ResultSet rs2 = connector.getQuerry(sql);
+        LinkedList<Subject> list = new LinkedList<Subject>();
+        DBConnector db = new DBConnector();
+        Subject_Data_Access sda= new Subject_Data_Access(db);
+        
+        while (rs2.next()) {
+            
+            String code = rs2.getString(2);
+            list.add(sda.getSubjectByCode(code));
+        }
+        lecturer.setSubjects(list);
 
         return lecturer;
     }
